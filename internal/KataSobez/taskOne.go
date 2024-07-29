ef950fd395d5ebc95f7c7e8f1bf98a3db5b37d9f
Dennis Mattews <alttert@mail.ru>
2024-07-30 01:04:33 +0300

    switch-case calc logic to KataSobez added

--- internal/KataSobez/taskOne.go
+++ internal/KataSobez/taskOne.go
@@ -17,30 +17,42 @@ func compare(a, b int) string {
 	return stringBigger
 }
 
-func newCalc(numA, numB int) int {
-	var sumAB int
+func newCalc(numA, numB, operation int) int {
+	var result int
 
-	switch i {
-	case 0:
-
-		sumAB = numA * numB
-	case 1:
-
-		sumAB = numA / numB
-	case 2:
-		sumAB = numA - numB
-	default:
-		sumAB = numA + numB
+	switch operation {
+	case 0: // Умножение
+		result = numA * numB
+	case 1: // Деление
+		if numB != 0 {
+			result = numA / numB
+		} else {
+			fmt.Println("Ошибка: Деление на ноль")
+			return 0 // Возвращаем 0 в случае ошибки
+		}
+	case 2: // Вычитание
+		result = numA - numB
+	default: // Сложение
+		result = numA + numB
 	}
+	return result
 }
 
 func main() {
-	var a, b, sumAB int
+	var a, b int
+	var operation int
+
 	fmt.Scanln(&a)
 	fmt.Scanln(&b)
 
-	res := (compare(a, b))
+	res := compare(a, b)
 	fmt.Println(res)
+
+	fmt.Println("Выберите операцию (0 - умножение, 1 - деление, 2 - вычитание, любое другое число - сложение):")
+	fmt.Scanln(&operation)
+
+	calcResult := newCalc(a, b, operation)
+	fmt.Printf("Результат операции: %d\n", calcResult)
 }
 
 // Необходимо написать функцию, которая принимает два числа в качестве аргументов и
