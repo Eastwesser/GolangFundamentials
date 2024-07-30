3daa499111654b1a7400e58b8afb1286c66622a3
Dennis Mattews <alttert@mail.ru>
2024-07-30 21:46:07 +0300

    additional task

--- internal/KataSobez/taskOne.go
+++ internal/KataSobez/taskOne.go
@@ -17,30 +17,30 @@ func compare(a, b int) string {
 	return stringBigger
 }
 
-func newCalc(numA, numB, operation int) int {
-	var result int
-
-	switch operation {
-	case 0: // Умножение
-		result = numA * numB
-	case 1: // Деление
-		if numB != 0 {
-			result = numA / numB
-		} else {
-			fmt.Println("Ошибка: Деление на ноль")
-			return 0 // Возвращаем 0 в случае ошибки
-		}
-	case 2: // Вычитание
-		result = numA - numB
-	default: // Сложение
-		result = numA + numB
-	}
-	return result
-}
+//func newCalc(numA, numB, operation int) int {
+//	var result int
+//
+//	switch operation {
+//	case 0: // Умножение
+//		result = numA * numB
+//	case 1: // Деление
+//		if numB != 0 {
+//			result = numA / numB
+//		} else {
+//			fmt.Println("Ошибка: Деление на ноль")
+//			return 0 // Возвращаем 0 в случае ошибки
+//		}
+//	case 2: // Вычитание
+//		result = numA - numB
+//	default: // Сложение
+//		result = numA + numB
+//	}
+//	return result
+//}
 
 func main() {
 	var a, b int
-	var operation int
+	//var operation int
 
 	fmt.Scanln(&a)
 	fmt.Scanln(&b)
@@ -48,11 +48,11 @@ func main() {
 	res := compare(a, b)
 	fmt.Println(res)
 
-	fmt.Println("Выберите операцию (0 - умножение, 1 - деление, 2 - вычитание, любое другое число - сложение):")
-	fmt.Scanln(&operation)
-
-	calcResult := newCalc(a, b, operation)
-	fmt.Printf("Результат операции: %d\n", calcResult)
+	//fmt.Println("Выберите операцию (0 - умножение, 1 - деление, 2 - вычитание, любое другое число - сложение):")
+	//fmt.Scanln(&operation)
+	//
+	//calcResult := newCalc(a, b, operation)
+	//fmt.Printf("Результат операции: %d\n", calcResult)
 }
 
 // Необходимо написать функцию, которая принимает два числа в качестве аргументов и
